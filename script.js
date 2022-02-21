fa3380bfe8d5854c1fb4e1534b15d22bc32e1b14
tatyanakoroliova130284 <tatyana.koroliova@gmail.com>
2022-02-21 21:05:53 +0300

    transform

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ const playSound = (event) => {
 };
 
 window.addEventListener("keydown", playSound);
+
+const removeTransition = (event) => {
+  console.log(event);
+  if (event.propertyName !== "transform") return;
+  event.target.classList.remove("transform");
+};
+const keys = document.querySelectorAll("keys__buttonstyle");
+keys.forEach((el) => el.addEventListener("transitionend", removeTransition));
