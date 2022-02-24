5351930590ee722cd000310d5e8ddeff588d35df
tatyanakoroliova130284 <tatyana.koroliova@gmail.com>
2022-02-24 20:05:49 +0300

    audio

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const playSound = (event) => {
   const button = document.querySelector(`button[data-key="${event.keyCode}"]`);
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+
   if (!audio) return;
   button.classList.add("transform");
   audio.currentTime = 0;
   audio.play();
-  console.log("ok");
 };
 
 window.addEventListener("keydown", playSound);
 
 const removeTransition = (event) => {
-  console.log(event);
   if (event.propertyName !== "transform") return;
   event.target.classList.remove("transform");
 };
-const keys = document.querySelectorAll("keys__buttonstyle");
+const keys = document.querySelectorAll(".keys__buttonstyle");
 keys.forEach((el) => el.addEventListener("transitionend", removeTransition));
+
+const playClickSound = (event) => {
+  const audio = document.querySelector(`audio[data-key ="${event.target.getAttribute("data-key")}"]`);
+
+  if (!audio) return;
+  event.target.classList.add("transform");
+  audio.currentTime = 0;
+  audio.play();
+};
+
+keys.forEach((el) => el.addEventListener("click", playClickSound));
